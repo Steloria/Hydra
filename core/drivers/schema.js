f4720147ebf50374afe27d9ebb94e9a0bd2fc377
Alexandre Simonet <droxy25@gmail.com>
2019-01-30 23:26:40 +0100

    Create schema class

---
+++ core/drivers/schema.js
@@ -0,0 +1,14 @@
+module.exports = class Schema {
+
+    constructor(name) {
+        this.name = name
+        this.tables = []
+    }
+
+    addTable() {}
+
+    deleteTable() {}
+
+    setTable() {}
+
+};
